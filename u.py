ec8d5f698f26fa5285c856d1651c0520a816606e
srijan upadhyay <jtm192168@iitd.ac.in>
2019-09-17 20:43:15 +0530

    rfe

---
+++ u.py
@@ -0,0 +1,9 @@
+from sys import *
+#import sys as s
+x=int(argv[1])
+y=int(argv[2])
+z=x+y
+print(z)
+print("")
+print("Sum of {0} and {1} is {2}".format(x,y,z))
+print("Sum of {1} and {0} is {2}".format(x,y,z))
